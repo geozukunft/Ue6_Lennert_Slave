9ffde17fc094a7e38e152e6336c5e184da5e9c9e
Viktor Lennert <geozukunft@outlook.at>
2020-03-21 15:12:23 +0100

    Projektdateien hinzufügen.

---
+++ Ue6_Lennert_Slave.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Ue6_Lennert_Slave.ino
+    Created:	27.02.2020 15:56:23
+    Author:     DESKTOP-DB8RJ3G\geozu
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+//void setup()
+//{
+//
+//
+//}
+//
+//// Add the main program code into the continuous loop() function
+//void loop()
+//{
+//
+//
+//}
